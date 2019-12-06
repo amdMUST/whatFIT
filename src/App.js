2e62ff44e3e1caa76c32ea39ae03b5a2d270a201
ahmed <52543589+amdMUST@users.noreply.github.com>
2019-12-06 18:31:15 -0500

    weather initial
    might make into reaction time game, installed everything but the return is not working in theGame.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import theGame from './theGame';
+
+function App() {
+
+  return (
+
+    <theGame />
+
+  )
+
+}
+
+export default App;
---
+++ src/theGame.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function theGame() {
+    return (
+        <div>
+            Test your reaction time
+        </div>
+    )
+}
