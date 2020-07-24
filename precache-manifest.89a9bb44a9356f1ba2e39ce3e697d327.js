588037f6b753f27a31d0ae6b8bbc54682982fde3
ahmed <52543589+amdMUST@users.noreply.github.com>
2020-07-23 21:21:44 -0400

    Updates

---
+++ precache-manifest.89a9bb44a9356f1ba2e39ce3e697d327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08150056e698207e300e19aa6eb5ea4e",
+    "url": "/whatFIT/index.html"
+  },
+  {
+    "revision": "8d1f22f337477f792047",
+    "url": "/whatFIT/static/css/main.d4e759c0.chunk.css"
+  },
+  {
+    "revision": "0304ebd5f80794499fe8",
+    "url": "/whatFIT/static/js/2.731a3930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whatFIT/static/js/2.731a3930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d1f22f337477f792047",
+    "url": "/whatFIT/static/js/main.b32216a2.chunk.js"
+  },
+  {
+    "revision": "4384a4cb9224313246df",
+    "url": "/whatFIT/static/js/runtime-main.710f5009.js"
+  }
+]);
